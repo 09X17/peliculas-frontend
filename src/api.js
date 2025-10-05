2a083f5dc2dbaeed883398cbc2e8209d1ba1deea
09X17 <72982944+09X17@users.noreply.github.com>
2025-10-05 11:16:34 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_URL || "http://localhost:4000/api";
+const baseURL = process.env.REACT_APP_API_URL || "https://apipeliculas-6end.onrender.com/api";
 
 const api = axios.create({ baseURL });
 
 export default api;
+
